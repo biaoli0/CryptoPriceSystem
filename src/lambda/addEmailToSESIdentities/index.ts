eabe61687bfc32bf9410010bb71e0c78334f873c
Biao Li <lbit123@outlook.com>
2024-11-29 10:39:16 +1100

    Revert "Use domain to send email"
    This reverts commit cb874a9ed705e2853730b8321761395e8ef8e9a4.

---
+++ src/lambda/addEmailToSESIdentities/index.ts
@@ -0,0 +1,48 @@
+
+import { APIGatewayEvent } from 'aws-lambda';
+import { SESClient, VerifyEmailIdentityCommand } from "@aws-sdk/client-ses";
+import { object, string } from 'yup';
+
+const queryParamsSchema = object({
+    email: string().email().required()
+});
+
+exports.handler = async (event: APIGatewayEvent) => {
+    try {
+        const { email } = await queryParamsSchema.validate(event.queryStringParameters);
+        const input = {
+            "EmailAddress": email
+        };
+        const client = new SESClient();
+        const command = new VerifyEmailIdentityCommand(input);
+        await client.send(command);
+
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                message: 'Verification email sent successfully',
+                note: 'Please check your email to complete verification',
+            })
+        };
+
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                message: 'Error adding email identity',
+                error
+            })
+        };
+    }
+};
+
+
